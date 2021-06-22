177cdc02ab223b232ee053cc9cfbccb267fe260a
jaya369 <jayaram886@gmail.com>
2021-06-22 09:34:26 +0530

    committed for demo33.py from jai_branch1

--- tests/demo33.py
+++ tests/demo33.py
@@ -1,8 +1,3 @@
-print('this is new file')
-
-print("this is added in june13th")
-print("hello world!!!")
-print("added by jayaram")
-
-print("some changes are done now in github file")
-print("hello")
+jdskbgjk
+sdjgbkjsb
+hello
